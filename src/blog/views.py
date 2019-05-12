5d4c395777a23c5a3e920b806bfb3e515f9cb1e5
pubuser7 <pubuser7@gmail.com>
2019-05-12 14:19:46 +0430

    add templates and some dummy views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def index(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
