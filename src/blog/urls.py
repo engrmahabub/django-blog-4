eb7e41cd9609f4f399c40075f797bc91810a03ba
pubuser7 <pubuser7@gmail.com>
2019-05-14 16:34:52 +0430

    add user posts and pagination

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+)
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user_posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
