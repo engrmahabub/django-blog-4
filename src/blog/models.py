5532e9fae979290c6d4c73b2ea8b553326d817d5
fun <pubuser7@gmail.com>
2019-05-19 15:57:18 +0430

    Costomize Admin page

--- src/blog/models.py
+++ src/blog/models.py
@@ -19,8 +19,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -34,4 +36,4 @@ class Comment(models.Model):
         return reverse("post_list")
 
     def __str__(self):
-        return self.text
+        return self.author
